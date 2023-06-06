feb3e301b74823da61397a7b5dfb5aa9f301b322
Arthur LC <arthur.lecoz00@gmail.com>
2023-06-06 23:25:06 +0200

    big commit
    many plots added

--- BEHAV_Sum_ProbesAnswers.py
+++ BEHAV_Sum_ProbesAnswers.py
@@ -24,19 +24,24 @@ fig_path = f"{root_path}/CGC_Pilots/Figs"
 
 # %%% Script
 
+inspect = False
+
 gong_dates = cfg.gong_dates
+df_l = []
+summary_data = {}
 
 for date in gong_dates :
     files = glob.glob(f"{behav_path}/*{date}*.csv")
     df_list = [pd.read_csv(file, delimiter = ";") for file in files]
     ID_list = [file[-8:-4] for file in files]
     
-    print("\n... Checking if shape is homogeneous among all dfs...")
-    for i, ID in enumerate(ID_list):
-        print(f"... {ID} report's length is : {len(df_list[i])}")
-    answer = input("Everything's Alright?\n(y/n)\n")
-    if answer == "n" :
-        sys.exit(0)
+    if inspect: 
+        print("\n... Checking if shape is homogeneous among all dfs...")
+        for i, ID in enumerate(ID_list):
+            print(f"... {ID} report's length is : {len(df_list[i])}")
+        answer = input("Everything's Alright?\n(y/n)\n")
+        if answer == "n" :
+            sys.exit(0)
     
     for i, df in enumerate(df_list) : 
         df.insert(0, "ID", [ID_list[i] for _ in range(len(df))])
@@ -46,11 +51,19 @@ for date in gong_dates :
     df_all = pd.concat(df_list)
     grouped_df = df_all.groupby(['n_probe', 'Mindstate'],
                                 as_index = False).size()
-    grouped_df["size"] = grouped_df["size"].div(len(df_all.ID.unique()))
+    grouped_df['size'] = np.asarray(grouped_df.groupby(
+        'n_probe')['size'].apply(lambda x: x / x.sum()))
     
-    # grouped_df = df_all.groupby(['n_probe', 'Mindstate']).size().unstack()
-    # # Normalize the counts to get proportions
-    # proportions_df = grouped_df.div(grouped_df.sum(axis=1), axis=0)
+    relative_percentage = grouped_df.groupby('Mindstate')['size'].sum() / grouped_df['size'].sum()
+    
+    summary_data[date] = relative_percentage
+
+    # Plot the simple proportions 
+    plt.figure(f"Proportions {date}", figsize = (16, 16))
+    sns.barplot(data = grouped_df, x = "Mindstate", y = 'size')
+    savename = f"{fig_path}/Proportion_Mindstate_{date}.png"
+    plt.savefig(savename, dpi = 300)
+    
     # # Plot the stacked bar plot
     fig, [ax1, ax2] = plt.subplots(2, 1, figsize = (16, 16))
     # proportions_df.plot(kind='bar', stacked=False, figsize=(10, 6), ax = ax1)
@@ -71,8 +84,24 @@ for date in gong_dates :
     savename = f"{fig_path}/Vigilance_Mindstate_{date}.png"
     plt.savefig(savename, dpi = 300)
     
-    
+    df_l.append(df_all)
+plt.close('all')
 
+summary_df = pd.DataFrame(summary_data)
+plt.figure("Proportions grandaverage", figsize = (16, 16))
+summary_df.reset_index().plot(x='Mindstate', kind='bar', figsize=(10, 6))
+plt.xlabel('Mindstate')
+plt.ylabel('Relative Percentage')
+plt.title('Relative Percentage of Mindstate Globally')
+savename = f"{fig_path}/summary_proportion_Mindstate.png"
+plt.savefig(savename, dpi = 300)
 
+mean_summary = summary_df.mean(axis=1)
+mean_summary.plot(kind='bar', figsize=(10, 6))
+plt.xlabel('Mindstate')
+plt.ylabel('Mean Relative Percentage')
+plt.title('Mean Relative Percentage of Mindstate')
+savename = f"{fig_path}/relativeper_proportion_Mindstate.png"
+plt.savefig(savename, dpi = 300)
 
 
